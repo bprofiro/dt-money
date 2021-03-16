94dcde92d02874b86a646c989e5379879e628884
Brenda <brendaprofiro@gmail.com>
2021-03-16 20:19:48 -0300

    chore: Configure global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>dt-money</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>dt-money</h1>
+      </div>
+
+      <GlobalStyle />
+    </>
   );
 }
 
